d3eedbdcf8d5b27a84b6c58c1bc26df8bad5924f
sayantansatpati <sayantan.satpati.sfbay@gmail.com>
2015-12-04 07:46:56 -0800

    Integrating Neighborhood with main (WIP)

--- yelp/js/neighborhood.js
+++ yelp/js/neighborhood.js
@@ -1,121 +1,135 @@
 // Base Elastic Search URL
-url_business_base = "http://54.183.182.71:9200/yelp/business/_search?size=2000"
+      url_business_base = "http://54.183.182.71:9200/yelp/business/_search?size=2000"
 
-//Width and height
-var w = 1400;
-var h = 200;
-var barPadding = 0.2;
-var margin = {top:100,bottom:100,right:100,left:100};	
+      function print_filter(filter){
+        var f=eval(filter);
+        if (typeof(f.length) != "undefined") {}else{}
+        if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
+        if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
+        console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+      } 
 
-var icon = L.icon({
-    iconUrl: './js/images/fork55.png',
-    //shadowUrl: 'leaf-shadow.png',
+      //Width and height
+      var w = 1400;
+      var h = 200;
+      var barPadding = 0.2;
+      var margin = {top:100,bottom:100,right:100,left:100}; 
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+      //Custom Icon
+      var neighborsIcon = L.icon({
+          iconUrl: 'img/fork55.png',
+          //shadowUrl: 'leaf-shadow.png',
 
+          iconSize:     [25, 40], // size of the icon
+          //shadowSize:   [50, 64], // size of the shadow
+          iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+          //shadowAnchor: [4, 62],  // the same for the shadow
+          popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
 
-function drawMarkerArea(data, center) {
-      var xf = crossfilter(data);
-      var groupname = "marker-area";
-      var business = xf.dimension(function(d) { return d.location; });
-      var businessGroup = business.group().reduceCount();
+      var myIcon = L.icon({
+          iconUrl: 'img/myrest.png',
+          //shadowUrl: 'leaf-shadow.png',
 
-      var marker = dc_leaflet.markerChart("#viz_neighborhood .map", groupname)
-          .dimension(business)
-          .group(businessGroup)
-          .width(600)
-          .height(400)
-          .center(center)
-          .zoom(12)
-          .renderPopup(true)
-          //.popup(function(d) { console.log(d); })
-          .filterByArea(true)
-          //.icon(icon);
+          iconSize:     [40, 75], // size of the icon
+          iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+          popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
 
-      /*
-      var types = xf.dimension(function(d) { return d.type; });
-      var typesGroup = types.group().reduceCount();
-
-      var pie = dc.pieChart("#demo2 .pie",groupname)
-          .dimension(types)
-          .group(typesGroup)
-          .width(200)
-          .height(200)
-          .renderLabel(true)
-          .renderTitle(true)
-          .ordering(function (p) {
-              return -p.value;
-          });
-
-      dc.renderAll(groupname);
-      return {marker: marker, pie: pie};
-     */
-     dc.renderAll(groupname);
-     return {marker: marker};
-
-  }
+      
+      /* Get the lat/lon */
+      url_1 = '{"query":{"match":{"business_id":"4bEjOyTaDG24SY5TxsaUNQ"}},"fields":["longitude","latitude","categories"]}'
 
 
-/* Get the lat/lon */
-url_1 = '{"query":{"match":{"business_id":"4bEjOyTaDG24SY5TxsaUNQ"}},"fields":["longitude","latitude","categories"]}'
+      var lat = null;
+      var lon = null;
+      var url_2 = null;
+
+      d3.json(url_business_base, function(error, d){
+        if (error) {  //If error is not null, something went wrong.
+              console.log(error);  //Log the error.
+            } else {      //If no error, the file loaded correctly. Yay!
+              console.log(d);
+            }
+
+            lat = d.hits.hits[0].fields.latitude[0]
+            lon = d.hits.hits[0].fields.longitude[0]
+            console.log(lon + ',' + lat);
+
+            var categories = d.hits.hits[0].fields.categories.join(' ')
+            console.log(categories);
 
 
-var lat = null;
-var lon = null;
-var url_2 = null;
+            /* Get neighborhood businesses */
+          
+          url_2 = '{"query":{"filtered":{"query":{"match":{"categories":{"query":"' + categories + '","minimum_should_match":1}}},"filter":{"geo_distance":{"distance":"1km","location":{"lat":' + lat + ',"lon":' + lon + '}}}}},"fields":["name","longitude","latitude","categories","stars"]}'
+          console.log(url_2);
 
-d3.json(url_business_base, function(error, d){
-	if (error) {  //If error is not null, something went wrong.
-      console.log(error);  //Log the error.
-    } else {      //If no error, the file loaded correctly. Yay!
-      console.log(d);
-  	}
+          d3.json(url_business_base, function(error, d){
+          if (error) {  //If error is not null, something went wrong.
+                console.log(error);  //Log the error.
+              } else {      //If no error, the file loaded correctly. Yay!
+                console.log('Total # Business in Neighborhood: ' + d.hits.total);
+              }
 
-  	lat = d.hits.hits[0].fields.latitude[0]
-  	lon = d.hits.hits[0].fields.longitude[0]
-  	console.log(lon + ',' + lat);
+            //Draw the Map
+            var map = L.map('map').setView([lat, lon], 15);
+            var circle = L.circle([lat,lon], 250, {
+              color: 'red',
+              fillColor: '#f03',
+              fillOpacity: 0.5
+          }).addTo(map);
 
-  	var categories = d.hits.hits[0].fields.categories.join(' ')
-  	console.log(categories);
+            // load a tile layer
+          L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGFsazJzYXlhbnRhbiIsImEiOiJjaWhyN29oeHUwMDRjdHNraGt2c2tnMGV0In0.UUa_E3pP4tUvc8JClZ2yBg', {
+              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+              maxZoom: 18,
+              id: 'talk2sayantan.ob6ndhba',
+              accessToken: 'pk.eyJ1IjoidGFsazJzYXlhbnRhbiIsImEiOiJjaWhyN29oeHUwMDRjdHNraGt2c2tnMGV0In0.UUa_E3pP4tUvc8JClZ2yBg'
+          }).addTo(map);
+
+          neighborhood = []
+
+            for(i in d.hits.hits) {
+              data = d.hits.hits[i].fields
+              //Add Markers on Map
+              var marker = L.marker([data.latitude[0], data.longitude[0]], {icon: neighborsIcon}).addTo(map);
+              marker.bindPopup("<b>" + data.name + ": " + data.stars + "</b>").openPopup();
+
+              neighborhood.push({'name': data.name[0], 'stars': data.stars[0], 'categories': data.categories})
+            }
+            console.log(neighborhood[0])
+
+            // Crossfilter
+            var ndx = crossfilter(neighborhood);
+            
+            var starsDim = ndx.dimension(function(d) { return d.stars; }); 
+            print_filter(starsDim.filter([1,2]));
+
+            var countByStars = starsDim.group().reduceSum(function(d) {return 1;});
+            print_filter(countByStars);
 
 
-  	/* Get neighborhood businesses */
-		
-		/*
-		url_2 = '{"query":{"filtered":{"filter":{"geo_distance":{"distance":"1km","location":{"lat":' + lat + ',"lon":' + lon + '}}}}},"fields":["name","longitude","latitude","categories"]}'
-		*/
+            chart = dc.barChart("#viz_neighborhood_stars");
+            chart
+              .width(600)
+              .height(300)
+              .x(d3.scale.ordinal())
+              .xUnits(dc.units.ordinal)
+              .elasticX(true)
+            .elasticY(true)
+              .brushOn(true)
+              .xAxisLabel("Star Ratings of Neighborhood Business")
+              .yAxisLabel("Count")
+              //.xAxis().tickFormat(function(d) {return day_of_week(d.day)})
+              .dimension(starsDim)
+              .group(countByStars)
+          chart.render();
 
-		url_2 = '{"query":{"filtered":{"query":{"match":{"categories":{"query":"' + categories + '","minimum_should_match":1}}},"filter":{"geo_distance":{"distance":"1km","location":{"lat":' + lat + ',"lon":' + lon + '}}}}},"fields":["name","longitude","latitude","categories","stars"]}'
-		console.log(url_2);
+            })
+              .header("Content-Type","application/json")
+            .send("POST", url_2)
 
-		d3.json(url_business_base, function(error, d){
-		if (error) {  //If error is not null, something went wrong.
-          console.log(error);  //Log the error.
-        } else {      //If no error, the file loaded correctly. Yay!
-          console.log('Total # Business in Neighborhood: ' + d.hits.total);
-      	}
-
-      	neighborhood = []
-
-			for(n in d.hits.hits) {
-				data = d.hits.hits[n].fields
-				neighborhood.push({'location': [data.latitude[0], data.longitude[0]], 'name': data.name, 'stars': data.stars})
-			}
-			console.log(neighborhood[1])
-
-			//Draw the Map
-			drawMarkerArea(neighborhood, [lat,lon]);
-
-      })
-		    .header("Content-Type","application/json")
-			.send("POST", url_2)
-
-  })
-	.header("Content-Type","application/json")
-	.send("POST", url_1)
-
+          })
+        .header("Content-Type","application/json")
+          .send("POST", url_1)
